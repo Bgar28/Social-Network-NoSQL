9a41615f3bb3458df6f7223f5b2556a613183736
Brian Garland <briangarlandk@gmail.com>
2022-02-11 21:17:12 -0600

    Reaction schema added to thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,4 @@
 const { Schema, model, Types } = require('mongoose');
-const reactionSchema = require('./Reaction');
 const moment = require('moment');
 
 // schema to create thought table
@@ -34,6 +33,35 @@ const thoughtSchema = new Schema(
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length
 })
+
+// schema to create reaction subdocument
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: (timestamp) => moment(timestamp).format('dddd, MMMM Do YYYY, h:mm:ss a') 
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
 
 const Thought = model('thought', thoughtSchema);
 module.exports = Thought;
